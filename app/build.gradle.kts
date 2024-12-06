8a5efdf3026a04b88c54e5d156cd6bb40d228472
acan12 <ary.suryawan@ottodigital.id>
2024-12-06 08:33:08 +0700

    update version library integrate with jetpack navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.jetpack.navigation)
 
     // core
     implementation(libs.coconut.mvvm)
@@ -109,6 +110,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    androidTestImplementation(libs.androidx.testing.navigation)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
--- app/src/main/java/com/beelabs/nyamnyam/ui/activity/OnBoardingActivity.kt
+++ app/src/main/java/com/beelabs/nyamnyam/ui/activity/OnBoardingActivity.kt
@@ -1,4 +1,15 @@
 package com.beelabs.nyamnyam.ui.activity
 
-class OnBoardingActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.beelabs.nyamnyam.databinding.ActivityOnboardingBinding
+
+class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
