22bf936c5a3955529617a19116f40034ac39a82b
acan12 <ary.suryawan@ottodigital.id>
2025-06-02 09:12:02 +0700

    update fragment inheritance from basefragment

--- app/src/main/java/com/beelabs/nyamnyam/ui/activity/onboard/fragment/BoardingSafeDeliveryFragment.kt
+++ app/src/main/java/com/beelabs/nyamnyam/ui/activity/onboard/fragment/BoardingSafeDeliveryFragment.kt
@@ -1,11 +1,8 @@
 package com.beelabs.nyamnyam.ui.activity.onboard.fragment
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import app.coconut2.coconut2_mvvm.base.BaseFragment
 import com.beelabs.nyamnyam.databinding.FragmentBoardingSafeDeliveryBinding
 
