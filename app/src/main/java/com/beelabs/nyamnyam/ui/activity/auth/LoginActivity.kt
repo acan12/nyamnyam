b9d44777627e8062da574dfde44046daaca72e42
acan12 <ary.suryawan@ottodigital.id>
2025-08-15 08:47:10 +0700

    add login activity

--- app/src/main/java/com/beelabs/nyamnyam/ui/activity/auth/LoginActivity.kt
+++ app/src/main/java/com/beelabs/nyamnyam/ui/activity/auth/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.beelabs.nyamnyam.ui.activity.auth
 
+import android.os.Bundle
 import app.coconut2.coconut2_mvvm.base.BaseActivity
-import com.beelabs.nyamnyam.databinding.ActivityOnboardingBinding
+import com.beelabs.nyamnyam.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginActivity : BaseActivity<ActivityOnboardingBinding>() {
-    override fun inflateBinding(): ActivityOnboardingBinding {
-        TODO("Not yet implemented")
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+    override fun inflateBinding(): ActivityLoginBinding =
+        ActivityLoginBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
     }
 }
