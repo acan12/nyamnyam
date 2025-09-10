c618c6eda4c4bdef53e9b81fc65f53e5465aa32b
arysuryawan <ary.suryawan@ottodigital.id>
2025-09-10 17:02:44 +0700

    added login layout component

--- app/src/main/java/com/beelabs/nyamnyam/ui/activity/auth/fragment/LoginFragment.kt
+++ app/src/main/java/com/beelabs/nyamnyam/ui/activity/auth/fragment/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.beelabs.nyamnyam.ui.activity.auth.fragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import app.coconut2.coconut2_mvvm.base.BaseFragment
 import com.beelabs.nyamnyam.databinding.FragmentLoginBinding
 
@@ -12,7 +13,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
     override fun setup() {
         binding.btnDemo.setOnClickListener {
-
+            Toast.makeText(requireActivity(), "Login Fragment", Toast.LENGTH_LONG).show()
         }
     }
 }
