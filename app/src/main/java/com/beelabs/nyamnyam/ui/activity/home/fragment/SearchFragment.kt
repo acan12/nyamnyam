a0d3c2d7f4007544d1a1a0d318bd688ec434b538
arysuryawan <ary.suryawan@ottodigital.id>
2025-10-13 14:08:27 +0700

    added loading lottie

--- app/src/main/java/com/beelabs/nyamnyam/ui/activity/home/fragment/SearchFragment.kt
+++ app/src/main/java/com/beelabs/nyamnyam/ui/activity/home/fragment/SearchFragment.kt
@@ -2,7 +2,9 @@ package com.beelabs.nyamnyam.ui.activity.home.fragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import app.coconut2.coconut2_mvvm.base.BaseFragment
+import com.beelabs.nyamnyam.R
 import com.beelabs.nyamnyam.databinding.FragmentSearchBinding
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
@@ -10,7 +12,14 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
         get() = FragmentSearchBinding::inflate
 
     override fun setup() {
+        binding.etInputSearch.setOnFocusChangeListener { view, b ->
+            if(b){
+                view.setBackgroundResource(R.drawable.rounded_white_transparent_stroke)
+                binding.lottieLoading.isVisible = false
+            }
+        }
 
+        binding.rvResultSearch
     }
 
 }
